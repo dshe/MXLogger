f73123552a4b7c0c0cd5b998baeadbd6cb163840
DavidS <david@tan.com>
2021-06-12 17:08:46 +0800

    add baseclass example

---
+++ MXLogger.xUnitTests/BaseTestExample.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MXLogger.xUnitTests
+{
+    public abstract class BaseTest
+    {
+        protected readonly ILogger Logger;
+
+        public BaseTest(ITestOutputHelper output)
+        {
+            Logger = new LoggerFactory()
+                .AddMXLogger(output.WriteLine)
+                .CreateLogger("Test");
+        }
+    }
+
+    public class Example : BaseTest
+    {
+        public Example(ITestOutputHelper output) : base(output) { }
+
+        [Fact]
+        public void Test()
+        {
+            Logger.LogInformation("message");
+        }
+    }
+}
